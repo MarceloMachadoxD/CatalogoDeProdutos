c394e43eb242c29b71642439bd8cf9e8d6a69aa7
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-12 19:59:44 -0300

    Initial Springboot

---
+++ backend/src/test/java/com/github/com/marcelomachadoxd/catalogodeprodutos/CatalogoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.com.marcelomachadoxd.catalogodeprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeProdutosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
