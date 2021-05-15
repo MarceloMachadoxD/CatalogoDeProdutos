b85405ce6dddaeabb7f06d333ea1a02010cb4504
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-15 12:12:24 -0300

    Entity Anotations

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/model/entities/Category.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/model/entities/Category.java
@@ -1,11 +1,17 @@
 package com.github.com.marcelomachadoxd.catalogodeprodutos.model.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name="tb_category")
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Category() {
