7ac307502a1e93d94b5c23060c4b4a159fd1de57
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-29 17:11:32 -0300

    Create a Test Object Factory

---
+++ backend/src/test/java/com/github/com/marcelomachadoxd/catalogodeprodutos/Factory.java
@@ -0,0 +1,22 @@
+package com.github.com.marcelomachadoxd.catalogodeprodutos;
+
+import com.github.com.marcelomachadoxd.catalogodeprodutos.DTO.ProductDTO;
+import com.github.com.marcelomachadoxd.catalogodeprodutos.model.entities.Category;
+import com.github.com.marcelomachadoxd.catalogodeprodutos.model.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product product = new Product(1L, "Phone Mock", "Mock", 800.0, "http://mock", Instant.parse("2020-07-13T20:50:07.12345Z"));
+        product.getCategories().add(new Category(2L, "Electronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO(){
+        Product product = createProduct();
+
+        return new ProductDTO(product, product.getCategories());
+    }
+}
