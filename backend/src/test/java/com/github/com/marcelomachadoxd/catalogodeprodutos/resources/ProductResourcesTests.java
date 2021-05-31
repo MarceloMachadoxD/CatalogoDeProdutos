60f813b466e592c49d1d678c592b180ec7654ef2
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-05-31 20:16:17 -0300

    Test findAllShouldReturnPage break call and assertions

--- backend/src/test/java/com/github/com/marcelomachadoxd/catalogodeprodutos/resources/ProductResourcesTests.java
+++ backend/src/test/java/com/github/com/marcelomachadoxd/catalogodeprodutos/resources/ProductResourcesTests.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
@@ -40,7 +42,11 @@ public class ProductResourcesTests {
 
     @Test
     public void findAllShouldReturnPage() throws Exception{
-        mockMvc.perform(get("/products")).andExpect(status().isOk());
+        ResultActions result = mockMvc.perform(get("/products")
+            .accept(MediaType.APPLICATION_JSON)
+        );
+
+        result.andExpect(status().isOk());
     }
 
 }
