dd18cac0f9f27c4551d8562ef9f82df0c99228c7
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-06 12:51:24 -0300

    Repository Annotation missing

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/repositories/ProductRepository.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/repositories/ProductRepository.java
@@ -2,6 +2,9 @@ package com.github.com.marcelomachadoxd.catalogodeprodutos.repositories;
 
 import com.github.com.marcelomachadoxd.catalogodeprodutos.model.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
