941785b7bd0e3818363c5c1d06f86d5f433a4546
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-13 18:51:15 -0300

    ReleaseSecurity config

---
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.github.com.marcelomachadoxd.catalogodeprodutos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers("/**");
+    }
+}
