d23de2fb1bf720097c8fb229741399d4490925f3
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-17 20:02:03 -0300

    ValidationError class to create a attribute to StandardError with a list of each error of FieldMessage

---
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/resources/exceptions/ValidationError.java
@@ -0,0 +1,18 @@
+package com.github.com.marcelomachadoxd.catalogodeprodutos.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message){
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+}
