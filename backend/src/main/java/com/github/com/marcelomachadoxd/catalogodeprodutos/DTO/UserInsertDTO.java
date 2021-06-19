d611b1f761e875c4277075dd19b6e323015e45dd
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-19 10:01:07 -0300

    Set UserInsertValid annotation

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/DTO/UserInsertDTO.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/DTO/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.github.com.marcelomachadoxd.catalogodeprodutos.DTO;
 
+import com.github.com.marcelomachadoxd.catalogodeprodutos.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
     private String password;
