12cbeb880c6481bbd04e5424119d705769caefdd
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-19 10:16:01 -0300

    UserUpdateDTO to validate Update

---
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/DTO/UserUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.github.com.marcelomachadoxd.catalogodeprodutos.DTO;
+
+import com.github.com.marcelomachadoxd.catalogodeprodutos.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO{
+
+
+}
