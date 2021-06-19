a6ef1fa660c602b3a0715e2919633f144aacd460
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-19 10:00:46 -0300

    Custom Constraint Validator

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/services/validation/UserInsertValidator.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/services/validation/UserInsertValidator.java
@@ -7,12 +7,17 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import com.github.com.marcelomachadoxd.catalogodeprodutos.DTO.UserInsertDTO;
+import com.github.com.marcelomachadoxd.catalogodeprodutos.model.entities.User;
+import com.github.com.marcelomachadoxd.catalogodeprodutos.repositories.UserRepository;
 import com.github.com.marcelomachadoxd.catalogodeprodutos.resources.exceptions.FieldMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 
 public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository repository;
 
     @Override
     public void initialize(UserInsertValid ann) {
@@ -23,7 +28,12 @@ public class UserInsertValidator implements ConstraintValidator<UserInsertValid,
 
         List<FieldMessage> list = new ArrayList<>();
 
-        // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+        User user = repository.findByEmail(dto.getEmail());
+        if (user != null){
+            list.add(new FieldMessage("email", "e-mail " + dto.getEmail() + " já cadastrado na base de dados"));
+        }
+
+
 
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
