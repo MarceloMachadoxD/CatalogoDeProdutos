5ba6c69202bf24c0ea505ba268a1ff831def521d
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-19 11:24:05 -0300

    UserUpdateValidator

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/services/validation/UserUpdateValidator.java
@@ -6,14 +6,20 @@ import com.github.com.marcelomachadoxd.catalogodeprodutos.model.entities.User;
 import com.github.com.marcelomachadoxd.catalogodeprodutos.repositories.UserRepository;
 import com.github.com.marcelomachadoxd.catalogodeprodutos.resources.exceptions.FieldMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Autowired
     private UserRepository repository;
@@ -25,11 +31,15 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
     @Override
     public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
 
+        var uriVars = (Map<String, String>)  request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long userId = Long.parseLong(uriVars.get("id"));
+
+
         List<FieldMessage> list = new ArrayList<>();
 
         User user = repository.findByEmail(dto.getEmail());
-        if (user != null){
-            list.add(new FieldMessage("email", "e-mail " + dto.getEmail() + " já cadastrado na base de dados"));
+        if (user != null && userId != user.getId()){
+            list.add(new FieldMessage("email", "e-mail " + dto.getEmail() + " já cadastrado na base de dados para outro usuário"));
         }
 
 
