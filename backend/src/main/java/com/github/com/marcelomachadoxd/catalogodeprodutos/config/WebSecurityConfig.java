3281f58da3f49c464d039b9432f5acb67890f6a9
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-20 10:03:55 -0300

    SpringSecurity config

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/config/WebSecurityConfig.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/config/WebSecurityConfig.java
@@ -1,16 +1,42 @@
 package com.github.com.marcelomachadoxd.catalogodeprodutos.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder);
+
+    }
+
     @Override
     public void configure(WebSecurity webSecurity) throws Exception {
-        webSecurity.ignoring().antMatchers("/**");
+        webSecurity.ignoring().antMatchers("/actuator/**");
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
