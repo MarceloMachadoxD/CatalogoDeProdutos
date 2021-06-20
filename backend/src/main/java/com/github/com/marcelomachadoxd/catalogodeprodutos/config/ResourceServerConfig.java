a9759c4f115a5e5e3225ed00b6b8e79f46700c27
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-06-20 17:17:31 -0300

    Enable H2-console route and frames

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/config/ResourceServerConfig.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.github.com.marcelomachadoxd.catalogodeprodutos.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,14 +11,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
 
@@ -33,6 +39,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //H2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
             .antMatchers(PUBLIC).permitAll()
             .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
