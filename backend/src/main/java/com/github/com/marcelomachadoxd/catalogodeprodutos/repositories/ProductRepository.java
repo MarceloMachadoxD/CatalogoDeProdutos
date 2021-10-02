a53240f9ea18c6a7828d0746fae24f6069a3a05e
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-10-02 16:04:23 -0300

    Fix n+1 select issue

--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/repositories/ProductRepository.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/repositories/ProductRepository.java
@@ -11,12 +11,14 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats " +
             "WHERE ( COALESCE(:categories) IS NULL OR cats IN :categories )" +
             "AND ( UPPER(obj.name) LIKE UPPER(CONCAT('%',:name,'%') )  )")
     Page<Product> find(List<Category> categories, String name, Pageable pageable);
+
+    @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj in :products") //fix n+1
+    List<Product> findProductsWithCategories(List<Product> products);
 
 }
--- backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/services/ProductService.java
+++ backend/src/main/java/com/github/com/marcelomachadoxd/catalogodeprodutos/services/ProductService.java
@@ -35,8 +35,8 @@ public class ProductService {
     public Page<ProductDTO>     findAllPaged(Long categoryId,String name, Pageable pageable) {
         List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
         Page<Product> list = productRepository.find(categories, name, pageable);
-
-        return list.map(ProductDTO::new);
+        productRepository.findProductsWithCategories(list.getContent()); //fix n+1
+        return list.map(x -> new ProductDTO(x, x.getCategories()) );
     }
 
     @Transactional(readOnly = true)
