1bfe5e4227cfd6988360a51734d8208dfd76fe0b
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-04 20:49:21 -0300

    navbar hamburguer menu

--- frontend/catalogodeprodutos/src/components/Navbar/index.tsx
+++ frontend/catalogodeprodutos/src/components/Navbar/index.tsx
@@ -1,17 +1,33 @@
-import './styles.css'
+import './styles.css';
+import 'bootstrap/js/src/collapse.js'
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md bg-primary main-nav">
-        <div className='container-fluid' >
+      <nav className="navbar navbar-expand-md bg-primary main-nav navbar-dark">
+        <div className="container-fluid">
           <a href="link" className="nav-logo-text">
             <h4>DS Catalog</h4>
           </a>
-          <div className='collapse navbar-collapse' >
-            <ul className='navbar-nav offset-md-2 main-menu' >
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#dscatalog-navbar"
+            aria-controls="dscatalog-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id='dscatalog-navbar'>
+            <ul className="navbar-nav offset-md-2 main-menu">
               <li>
-                <a href="link" className='active'>HOME</a>
+                <a href="link" className="active">
+                  HOME
+                </a>
               </li>
               <li>
                 <a href="link">CATÁLOGO</a>
