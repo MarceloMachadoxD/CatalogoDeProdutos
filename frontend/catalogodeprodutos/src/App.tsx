d6f89b32d0512a156d8f6eb3c3879aba576be7bb
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-04 19:51:22 -0300

    Component Navbar

--- frontend/catalogodeprodutos/src/App.tsx
+++ frontend/catalogodeprodutos/src/App.tsx
@@ -1,10 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
     <>
-      <h1>basic</h1>
+      <Navbar />
     </>
   );
 }
