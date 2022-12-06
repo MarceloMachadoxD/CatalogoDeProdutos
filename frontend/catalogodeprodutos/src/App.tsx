3c5bce58382208d8c9ff74bbcf410509464767b6
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-06 11:40:07 -0300

    finish responsivity

--- frontend/catalogodeprodutos/src/App.tsx
+++ frontend/catalogodeprodutos/src/App.tsx
@@ -1,11 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Home />
     </>
   );
 }
---
+++ frontend/catalogodeprodutos/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+  return (
+    <>
+      <button className="btn btn-primary btn-icon">
+        <h6>Inicie agora sua busca</h6>
+      </button>
+    </>
+  );
+};
+
+export default ButtonIcon;
