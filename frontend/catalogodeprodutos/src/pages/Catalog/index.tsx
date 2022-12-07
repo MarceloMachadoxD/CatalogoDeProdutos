0f2a3ca93d4462a1e2d82d7e260ef32f82e7f915
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-07 15:05:06 -0300

    adjust catalog screen

--- frontend/catalogodeprodutos/src/pages/Catalog/index.tsx
+++ frontend/catalogodeprodutos/src/pages/Catalog/index.tsx
@@ -1,29 +1,27 @@
-import Navbar from 'components/Navbar';
 import ProductCard from 'components/ProductCard';
 import './styles.css';
 
 const Catalog = () => {
   return (
     <>
-      <h1>Tela de Catalog</h1>
       <div className="container my-4">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 ">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 ">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 ">
+          <div className="col-sm-6 col-lg-4 col-xl-3">
             <ProductCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3">
             <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 ">
+          <div className="col-sm-6 col-lg-4 col-xl-3">
             <ProductCard />
           </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 ">
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3">
             <ProductCard />
           </div>
         </div>
