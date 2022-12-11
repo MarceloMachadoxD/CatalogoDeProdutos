18ae762e6a7a9a64661601c795c6b904a7549b00
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-11 16:39:11 -0300

    format price

---
+++ frontend/catalogodeprodutos/src/util/formaters.ts
@@ -0,0 +1,4 @@
+export const  formatPrice = (price: number) => {
+    const params = {minimumFractionDigits: 2, maximumFractionDigits: 2}
+    return new Intl.NumberFormat('pt-BR', params).format(price);
+}
