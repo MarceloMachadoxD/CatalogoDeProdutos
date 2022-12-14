9359fb54fd18208bc966325f175ee497b339c6ac
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-14 20:46:01 -0300

    Catalog Title

--- frontend/catalogodeprodutos/src/pages/Catalog/index.tsx
+++ frontend/catalogodeprodutos/src/pages/Catalog/index.tsx
@@ -23,7 +23,11 @@ const product: Product = {
 const Catalog = () => {
   return (
     <>
-      <div className="container my-4">
+      <div className="container my-4 catalog-container">
+        <div className='row catalog-title-container'>
+          <h1>Catalogo de Produtos</h1>
+
+        </div>
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3">
             <Link to="/products/1">
