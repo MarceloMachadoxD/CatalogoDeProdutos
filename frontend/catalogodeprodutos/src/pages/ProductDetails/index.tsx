6693c8f61f2876ac36add9a8362bbafdee3a293e
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-14 19:01:43 -0300

    estrutura basica product details

--- frontend/catalogodeprodutos/src/pages/ProductDetails/index.tsx
+++ frontend/catalogodeprodutos/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,39 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow-image.svg';
+import ProductPrice from 'components/ProductPrice';
+
 const ProductDetails = () => {
-    return(
-        <>
-        <h1>Product Details</h1>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="product-details-container">
+        <div className="product-detais-card">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>VOLTAR</h2>
+          </div>
+          <div className="row">
+            <div className="col-xl-6">
+              <div className="img-container">
+                <img
+                  src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
+                  alt="nome do produto"
+                />
+              </div>
+              <div className="name-price-container">
+                <h1>Nome do produto</h1>
+                <ProductPrice price={123.4} />
+              </div>
+            </div>
+            <div className="col-xl-6">
+              <div className="description-container">
+                <h2>DESCRIÇÃO DO PRODUTO</h2>
+                <p>UIAHUIahIUAHIUH adhoashdlahsd AODHAOUHDUADKJASBDKAJSBD akjasdkjabhskjdbaksdb</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ProductDetails;
