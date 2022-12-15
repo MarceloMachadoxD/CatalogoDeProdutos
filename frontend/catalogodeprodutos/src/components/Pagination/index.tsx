3b220486b0b5abf449481ee421280a1073197e9e
MarceloMachadoxD <infomachado1987@gmail.com>
2022-12-15 07:23:43 -0300

    pagination styling

---
+++ frontend/catalogodeprodutos/src/components/Pagination/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow-image.svg';
+
+const Pagination = () => {
+  return (
+    <>
+      <div className="pagination-container">
+        <ArrowIcon className='arrow-previous arrow-inactive' />
+        <div className="pagination-item active">1</div>
+        <div className="pagination-item">2</div>
+        <div className="pagination-item">3</div>
+        <div className="pagination-item">...</div>
+        <div className="pagination-item">10</div>
+        <ArrowIcon className='arrow-next arrow-active' />
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
--- frontend/catalogodeprodutos/src/pages/Catalog/index.tsx
+++ frontend/catalogodeprodutos/src/pages/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
@@ -24,16 +25,18 @@ const Catalog = () => {
   return (
     <>
       <div className="container my-4 catalog-container">
-        <div className='row catalog-title-container'>
+        <div className="row catalog-title-container">
           <h1>Catalogo de Produtos</h1>
-
         </div>
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3">
             <Link to="/products/1">
-            <ProductCard product={product} />
+              <ProductCard product={product} />
             </Link>
           </div>
+        </div>
+        <div className="row">
+          <Pagination />
         </div>
       </div>
     </>
