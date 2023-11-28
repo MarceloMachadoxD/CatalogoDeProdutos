e9baafc85684e74a85ccdc4b4f536a5c85800ca1
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-11-28 07:38:37 -0300

    Add styles.css import to Auth component auth-banner-container style

--- frontend/catalogodeprodutos/src/pages/Admin/Auth/index.tsx
+++ frontend/catalogodeprodutos/src/pages/Admin/Auth/index.tsx
@@ -1,6 +1,8 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
 
+import './styles.css';
+
 const Auth = () => {
   return (
     <div className="auth-container">
