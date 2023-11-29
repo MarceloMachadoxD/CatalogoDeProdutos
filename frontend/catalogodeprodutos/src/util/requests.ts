df4ae4d3080ad1dda4fb2e1ea89d6862bfd18d74
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-11-29 19:57:48 -0300

    Add client ID and client secret to requests.ts

--- frontend/catalogodeprodutos/src/util/requests.ts
+++ frontend/catalogodeprodutos/src/util/requests.ts
@@ -1,2 +1,6 @@
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
+
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
