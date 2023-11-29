1a88581a17427d66095caec190e9c573914cb1ee
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-11-29 19:12:09 -0300

    Add react-hook-form dependency for form handling add test on login form

--- frontend/catalogodeprodutos/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/catalogodeprodutos/src/pages/Admin/Auth/Login/index.tsx
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
+import { useForm } from 'react-hook-form';
 
 import './styles.css';
 
+type FormData = {
+  username: string;
+  password: string;
+};
+
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+  const onSubmit = (formData : FormData) => {
+    console.log(formData);
+  };
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
+            {...register('username')}
             type="text"
             className="form-control base-input"
             placeholder="Email"
@@ -18,6 +29,7 @@ const Login = () => {
         </div>
         <div className="mb-2">
           <input
+            {...register('password')}
             type="password"
             className="form-control base-input "
             placeholder="Password"
