1171f52baa8df26447d42d770036af39abfc7761
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-12-01 21:02:30 -0300

    Remove unnecessary imports and add PrivateRoute component

--- frontend/catalogodeprodutos/src/pages/Admin/index.tsx
+++ frontend/catalogodeprodutos/src/pages/Admin/index.tsx
@@ -1,8 +1,9 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 
 import './styles.css';
 import Users from './User';
+import PrivateRoute from 'components/PrivateRoute';
 
 const Admin = () => {
   return (
@@ -10,15 +11,15 @@ const Admin = () => {
       <Navbar />
       <div className="admin-content">
         <Switch>
-          <Route path="/admin/products">
+          <PrivateRoute path="/admin/products">
             <h1>Product CRUD</h1>
-          </Route>
-          <Route path="/admin/categories">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/categories">
             <h1>Category CRUD</h1>
-          </Route>
-          <Route path="/admin/users">
+          </PrivateRoute>
+          <PrivateRoute path="/admin/users">
             <Users />
-          </Route>
+          </PrivateRoute>
         </Switch>
       </div>
     </div>
