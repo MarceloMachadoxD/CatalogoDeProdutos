e98ca846b10307e63716f7381d4015cfaec82f12
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-12-01 21:03:51 -0300

    Remove isAuthenticated function call from Home

--- frontend/catalogodeprodutos/src/pages/Home/index.tsx
+++ frontend/catalogodeprodutos/src/pages/Home/index.tsx
@@ -3,12 +3,10 @@ import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
-import { isAuthenticated } from 'util/requests';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>{isAuthenticated() ? 'autenticado' : 'não autenticado'}</h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
