a4fde6734a553273284835640daa9a823667491c
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-12-02 21:11:54 -0300

    Refactor PrivateRoute component to handle role-based authentication

--- frontend/catalogodeprodutos/src/components/PrivateRoute/index.tsx
+++ frontend/catalogodeprodutos/src/components/PrivateRoute/index.tsx
@@ -1,22 +1,29 @@
 import { Redirect, Route } from 'react-router-dom';
-import { isAuthenticated } from 'util/requests';
+import { Role, hasAnyRoles, isAuthenticated } from 'util/requests';
 
 type Props = {
   children: React.ReactNode;
   path: string;
+  roles?: Role[];
 };
 
-const PrivateRoute = ({ children, path }: Props) => {
+const PrivateRoute = ({ children, path, roles = [] }: Props) => {
   return (
     <Route
       path={path}
-      render={({
-        location
-      }) =>
-        isAuthenticated() ? children: <Redirect to={{
-            pathname: "/admin/auth/login",
-            state: { from: location }
-        }} />
+      render={({ location }) =>
+        !isAuthenticated() ? (
+          <Redirect
+            to={{
+              pathname: '/admin/auth/login',
+              state: { from: location },
+            }}
+          />
+        ) : !hasAnyRoles(roles) ? (
+          <Redirect to={{ pathname: '/admin/products' }} />
+        ) : (
+          children
+        )
       }
     />
   );
